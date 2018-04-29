c9d06667c7c33394fefd1e3b64d08da61e2964dd
Dennis Lord <lorddennis1006@gmail.com>
2018-04-29 22:50:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,8 +12,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 
 def words_five_letters_long(text)
     text.scan(/\b\w{5}\b/)
-  end	
-end
+  end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
